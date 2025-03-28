532032933bb629e2a25357561570debdfa074348
CHF <xronisefstathiadis@hotmail.com>
2025-03-28 20:15:04 +0200

    gun_test

--- scripts/soldier_character.gd
+++ scripts/soldier_character.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-var movespeed = 500
+var movespeed = 800
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	velocity = input_dir.normalized() * movespeed
 	move_and_slide()
 	look_at(get_global_mouse_position())
+	
+	
