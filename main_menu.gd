f3c691094be1c07b69fdcbf8b9e3f79cf5433c71
akistsamp <akistsamp03@gmail.com>
2025-04-03 13:06:18 +0300

    132

--- main_menu.gd
+++ main_menu.gd
@@ -4,6 +4,8 @@ extends Node2D
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://SCENES/game.tscn")
 
+func _on_exit_pressed() -> void:
+	get_tree().quit()
 
 func _on_info_pressed() -> void:
-	get_tree().change_scene_to_file("res://info_menu.tscn")
+	get_tree().change_scene_to_file.bind("res://info_menu.tscn")
