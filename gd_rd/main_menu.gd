e9cd4befdc3cc725dc3de7948f6a8aff1a83c06f
akistsamp <akistsamp03@gmail.com>
2025-04-09 17:55:38 +0300

    123123

--- gd_rd/main_menu.gd
+++ gd_rd/main_menu.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 
 func _on_start_pressed() -> void:
+	$click.play()
 	get_tree().change_scene_to_file("res://SCENES/game.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
-func _on_info_pressed() -> void:
+func _on_infooo_pressed() -> void:
 	get_tree().change_scene_to_file("res://SCENES/infomenu.tscn")
+
+func _on_start_mouse_entered() -> void:
+	$hover.play()
+
+func _on_exit_mouse_entered() -> void:
+	$hover.play()
+
+
+func _on_infooo_mouse_entered() -> void:
+	$hover.play()
