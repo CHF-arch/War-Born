e6ea009a92ee96d82bd6534bcb15ca27ad3d2316
CHF <xronisefstathiadis@hotmail.com>
2025-04-13 09:52:33 +0300

    spawn_try

--- scripts/spawn.gd
+++ scripts/spawn.gd
@@ -2,16 +2,17 @@
 extends Node2D
 
 var enemy_scene = preload("res://SCENES/enemy_1.tscn")
-@export var spawn_count: int = 10    
-@export var spawn_radius: float = 100  
+var spawn_count: int = 10    
+#@export var spawn_radius: float = 100
 
 func _ready():
 	spawn_enemies()
 
 func spawn_enemies():
-	var plane_exit_position = Vector2(-500, 500)  # Συντεταγμένες εξόδου αεροπλάνου
 	for i in range(spawn_count):
 		var enemy = enemy_scene.instantiate()
-		enemy.global_position = plane_exit_position  # Ίδια θέση για όλους
 		get_parent().add_child(enemy)
-		print("Spawned enemy at: ", enemy.global_position)  # Debug
+		enemy.modulate = Color.RED
+		enemy.global_position = Vector2(3196, 2408)
+		print("Spawned enemy at: ", enemy.global_position)  
+		print("Enemy position: ", global_position, " | Visible: ", is_visible_in_tree())
